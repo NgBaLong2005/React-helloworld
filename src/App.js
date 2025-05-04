3ac1efdddd38f39ae939dcd021e0eb200688a3ce
Nguyen Ba Long <ngbalong2005@gmail.com>
2025-05-04 15:09:42 +0700

    event

--- src/App.js
+++ src/App.js
@@ -3,26 +3,50 @@ import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
 import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
 import MyComponent from "./component/MyComponent";
+import React from "react";
 
-const App = () => {
-  const count = useSelector((state) => state.counter.count);
-  const dispatch = useDispatch();
+class App extends React.Component {
+  handleClick(event) {
+    // console.log("Long nguyen");
+    console.log(event);
+  }
+  handleOnMoverOver(event) {
+    console.log(event);
+  }
+  state = {
+    name: "long",
+    age: 20,
+  };
 
-  return (
-    <div>
-      hello world
-      <MyComponent></MyComponent>
-    </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>Hello world</p>
-    //     <div>Count = {count}</div>
-    //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-    //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-    //   </header>
-    // </div>
-  );
-};
-
+  render() {
+    return (
+      <div>
+        my name is {this.state.name} and i am {this.state.age} year old
+        <br></br>
+        <button onClick={this.handleClick}>click me</button>
+        <br></br>
+        <button onMouseOver={this.handleOnMoverOver}>hover me</button>
+      </div>
+    );
+  }
+}
 export default App;
+
+// const App = () => {
+//   const count = useSelector((state) => state.counter.count);
+//   const dispatch = useDispatch();
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>Hello world</p>
+//         <div>Count = {count}</div>
+//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+//       </header>
+//     </div>
+//   );
+// };
+
+// export default App;
--- src/component/MyComponent.js
+++ src/component/MyComponent.js
@@ -1,10 +1,1 @@
-import React from "react";
 
-class MyComponent extends React.Component {
-  // JSX
-  render() {
-    return <div>my first component</div>;
-  }
-}
-
-export default MyComponent;
